8dd042188f9d1757a6d05fdc1c4fc8bed88708f0
Meshal <meshal.aneze@gmail.com>
2022-02-13 16:20:56 +0300

    heroies service list

---
+++ src/variables/heroes.js
@@ -0,0 +1,17 @@
+const heroes = [{
+        "name": "Meshal Saud",
+        "powers": ["coding with python", "coding with javaScript"],
+        "rate": 4
+    },
+    {
+        "name": "Khaled",
+        "powers": ["coding with java", "coding with react"],
+        "rate": 3
+    },
+    {
+        "name": "Waleed",
+        "powers": ["coding with GoLang", "Design with XD"],
+        "rate": 5
+    },
+
+]
