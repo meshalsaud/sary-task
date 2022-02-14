e81a22c6ba6a9093f99a81210d9abbb092f5cced
Meshal <meshal.aneze@gmail.com>
2022-02-14 22:59:49 +0300

    powers badge fixes

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -81,7 +81,7 @@ export const Table = ({ data }) => {
                 </tr>
               </thead>
               <tbody>
-                {Array(3)
+                {Array(10)
                   .fill()
                   .map(index => {
                     return (
@@ -103,13 +103,21 @@ export const Table = ({ data }) => {
                           <p className="text-gray-900 whitespace-no-wrap">Meshal Saud</p>
                         </td>
                         <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                          <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                            <span
-                              aria-hidden="true"
-                              className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
-                            ></span>
-                            <span className="relative">active</span>
-                          </span>
+                          <div className="flex flex-wrap max-w-xs space-x-2">
+                            {Array(3)
+                              .fill()
+                              .map((index) => {
+                                return (
+                                <span key={index} className="relative my-1 mx-2 inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                  <span
+                                    aria-hidden="true"
+                                    className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+                                  ></span>
+                                  <span className="relative">active</span>
+                                </span>
+                                )
+                              })}
+                          </div>
                         </td>
                         <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                           <a href="#" className="flex items-center space-x-2">
@@ -118,10 +126,18 @@ export const Table = ({ data }) => {
                           </a>
                         </td>
                         <td className="px-5 py-5 space-x-3 border-b border-gray-200 bg-white text-sm">
-                          <button onClick={() => setShowProfileModal(!showProfileModal)} href="#" className="inline-block outline-none">
+                          <button
+                            onClick={() => setShowProfileModal(!showProfileModal)}
+                            href="#"
+                            className="inline-block outline-none"
+                          >
                             <GrOverview className="h-5 w-5" />
                           </button>
-                          <button onClick={() => setShowCallHeroModal(!showCallHeroModal)} href="#" className="inline-block outline-none">
+                          <button
+                            onClick={() => setShowCallHeroModal(!showCallHeroModal)}
+                            href="#"
+                            className="inline-block outline-none"
+                          >
                             <GiBugleCall fill="#4F45BA" className="h-5 w-5 animate-pulse" />
                           </button>
                         </td>
@@ -130,64 +146,7 @@ export const Table = ({ data }) => {
                   })}
               </tbody>
             </table>
-            <div className="px-5 bg-white py-5 flex flex-col xs:flex-row items-center xs:justify-between">
-              <div className="flex items-center">
-                <button
-                  type="button"
-                  className="w-full p-4 border text-base rounded-l-xl text-gray-600 bg-white hover:bg-gray-100"
-                >
-                  <svg
-                    width="9"
-                    fill="currentColor"
-                    height="8"
-                    className=""
-                    viewBox="0 0 1792 1792"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z"></path>
-                  </svg>
-                </button>
-                <button
-                  type="button"
-                  className="w-full px-4 py-2 border-t border-b bg-secondary-0 text-base text-white  hover:bg-gray-100 "
-                >
-                  1
-                </button>
-                <button
-                  type="button"
-                  className="w-full px-4 py-2 border text-base text-gray-600 bg-white hover:bg-gray-100"
-                >
-                  2
-                </button>
-                <button
-                  type="button"
-                  className="w-full px-4 py-2 border-t border-b text-base text-gray-600 bg-white hover:bg-gray-100"
-                >
-                  3
-                </button>
-                <button
-                  type="button"
-                  className="w-full px-4 py-2 border text-base text-gray-600 bg-white hover:bg-gray-100"
-                >
-                  4
-                </button>
-                <button
-                  type="button"
-                  className="w-full p-4 border-t border-b border-r text-base  rounded-r-xl text-gray-600 bg-white hover:bg-gray-100"
-                >
-                  <svg
-                    width="9"
-                    fill="currentColor"
-                    height="8"
-                    className=""
-                    viewBox="0 0 1792 1792"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z"></path>
-                  </svg>
-                </button>
-              </div>
-            </div>
+       
           </div>
         </div>
       </div>
